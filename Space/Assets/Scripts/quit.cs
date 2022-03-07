fcecd9f5e969ea7e8f29cc0ac6b1996937b1f0ae
WladDMG <wladdmg@gmail.com>
2022-03-07 06:36:35 -0300

    telas e caixa de textos

---
+++ Space/Assets/Scripts/quit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class quit : MonoBehaviour
+{
+   public void Quit()
+    {
+        Application.Quit();
+    }
+}
